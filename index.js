0f3846e02ce28fafea76a85cfa07f70b700ec1d1
1Rendes <1admin@ukr.net>
2024-06-04 16:53:20 +0200

    modals-js

---
+++ index.js
@@ -0,0 +1,51 @@
+const data = [
+  ".backdrop",
+  ".mob-close-button",
+  ".close-button",
+  ".close-butt-icon",
+  ".close-butt-use",
+];
+const orderButton = document.querySelector("[data-open-form]");
+const menuButton = document.querySelector("[data-open-mobile]");
+const closers = document.querySelectorAll(data);
+
+document.addEventListener("click", closeHandler);
+menuButton.addEventListener("click", () => closers[0].classList.add("is-open"));
+orderButton.addEventListener("click", () =>
+  closers[4].classList.add("is-open")
+);
+function closeHandler(event) {
+  const element = event.target.classList[0];
+  if (data.includes("." + element)) {
+    closers.forEach((element) => {
+      if (element.classList.contains("is-open")) {
+        element.classList.remove("is-open");
+      }
+    });
+  }
+}
+// const orderButton = document.querySelector(".order-button");
+// const backdrop = document.querySelector(".backdrop");
+// const closeButton = document.querySelector(".close-button");
+// const mobMenuRef = document.querySelector(".mob-menu-ref");
+// const backdropMob = document.querySelector(".backdrop-mob");
+// const mobCloseButton = document.querySelector(".mob-close-button");
+
+// backdrop.addEventListener("click", () => {
+//   backdrop.classList.remove("is-open");
+// });
+// orderButton.addEventListener("click", () => {
+//   backdrop.classList.add("is-open");
+// });
+// closeButton.addEventListener("click", () => {
+//   backdrop.classList.remove("is-open");
+// });
+// mobMenuRef.addEventListener("click", () => {
+//   backdropMob.classList.add("is-open");
+// });
+// backdropMob.addEventListener("click", () => {
+//   backdropMob.classList.remove("is-open");
+// });
+// mobCloseButton.addEventListener("click", () => {
+//   backdropMob.classList.remove("is-open");
+// });
